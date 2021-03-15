1697508c1915c3eb31e43a4ccc6dc7bda50bbb80
Jorge Mayoral Álvarez <y0rch.developer@gmail.com>
2021-03-15 12:03:16 +0100

    fix: :bug: Footer fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,12 @@
-import { Box, Center, Link, Text } from '@chakra-ui/layout';
+import { Box, Center, Link, Text, Flex } from '@chakra-ui/layout';
 
 const Footer = () => {
   return (
-    <Box my={2}>
-      <Center>
-        <Link href="https://yorch.dev/contacto" isExternal>
-          <Text>Contact</Text>
-        </Link>
-      </Center>
-    </Box>
+    <Center bgColor="#fff" p={4}>
+      <Link href="https://yorch.dev/contacto" isExternal>
+        <Text>Contact</Text>
+      </Link>
+    </Center>
   );
 };
 
